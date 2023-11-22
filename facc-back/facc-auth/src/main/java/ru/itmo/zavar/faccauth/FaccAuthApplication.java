39b1c188997d7d7e5b6308faf2ab9f2e6c1f8eda
Zavar30 <yarusking@mail.ru>
2023-11-22 16:41:50 +0300

    initial commit

---
+++ facc-back/facc-auth/src/main/java/ru/itmo/zavar/faccauth/FaccAuthApplication.java
@@ -0,0 +1,13 @@
+package ru.itmo.zavar.faccauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaccAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaccAuthApplication.class, args);
+	}
+
+}
