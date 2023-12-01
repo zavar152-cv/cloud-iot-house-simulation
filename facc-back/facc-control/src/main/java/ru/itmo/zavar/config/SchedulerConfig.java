c16ed0532bb7144fc30533753d2f397959e19010
Zavar30 <yarusking@mail.ru>
2023-12-01 19:13:04 +0300

    initial commit for control-service

---
+++ facc-back/facc-control/src/main/java/ru/itmo/zavar/config/SchedulerConfig.java
@@ -0,0 +1,37 @@
+package ru.itmo.zavar.config;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.quartz.QuartzProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.quartz.SchedulerFactoryBean;
+
+@RequiredArgsConstructor
+@Configuration
+public class SchedulerConfig {
+
+    private final ApplicationContext applicationContext;
+    private final QuartzProperties quartzProperties;
+
+    @Bean
+    public SchedulerFactoryBean schedulerFactoryBean() {
+
+        SchedulerJobFactory jobFactory = new SchedulerJobFactory();
+        jobFactory.setApplicationContext(applicationContext);
+
+        Properties properties = new Properties();
+        properties.putAll(quartzProperties.getProperties());
+
+        SchedulerFactoryBean factory = new SchedulerFactoryBean();
+        factory.setOverwriteExistingJobs(true);
+        factory.setQuartzProperties(properties);
+        factory.setJobFactory(jobFactory);
+        return factory;
+    }
+}
---
+++ facc-back/facc-control/src/main/java/ru/itmo/zavar/config/SchedulerJobFactory.java
@@ -0,0 +1,25 @@
+package ru.itmo.zavar.config;
+
+import org.quartz.spi.TriggerFiredBundle;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.lang.NonNull;
+import org.springframework.scheduling.quartz.SpringBeanJobFactory;
+
+public class SchedulerJobFactory extends SpringBeanJobFactory implements ApplicationContextAware {
+
+    private AutowireCapableBeanFactory beanFactory;
+
+    @Override
+    public void setApplicationContext(final ApplicationContext context) {
+        beanFactory = context.getAutowireCapableBeanFactory();
+    }
+
+    @Override
+    protected @NonNull Object createJobInstance(@NonNull TriggerFiredBundle bundle) throws Exception {
+        final Object job = super.createJobInstance(bundle);
+        beanFactory.autowireBean(job);
+        return job;
+    }
+}
