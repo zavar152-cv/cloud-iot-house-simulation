525f6a58d62cb29b616c6c24e9c59edfccbe1d2e
Zavar30 <yarusking@mail.ru>
2023-12-01 19:13:04 +0300

    initial commit for control-service

---
+++ facc-back/facc-control/src/main/java/ru/itmo/zavar/FaccControlApplication.java
@@ -0,0 +1,13 @@
+package ru.itmo.zavar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaccControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaccControlApplication.class, args);
+	}
+
+}
