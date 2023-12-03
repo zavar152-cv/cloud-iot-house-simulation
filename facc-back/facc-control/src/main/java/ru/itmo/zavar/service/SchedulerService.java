12bc8f1201bcd5554e0d9447c0b6ca3eb4fdb27c
Zavar30 <yarusking@mail.ru>
2023-12-03 19:39:01 +0300

    implement all methods for SchedulerService, implement all methods for SchedulerController according to REST naming convention, improve logging messages

--- facc-back/facc-control/src/main/java/ru/itmo/zavar/service/SchedulerService.java
+++ facc-back/facc-control/src/main/java/ru/itmo/zavar/service/SchedulerService.java
@@ -1,19 +1,26 @@
 package ru.itmo.zavar.service;
 
+import org.quartz.SchedulerException;
+import ru.itmo.zavar.dto.TimetableEntryDTO;
 import ru.itmo.zavar.util.JobGroup;
 
+import java.util.List;
 import java.util.NoSuchElementException;
 
 public interface SchedulerService {
-    void createNewTimetableEntry(String name, JobGroup group, String cronExpression, String description);
+    void createTimetableEntry(String name, JobGroup group, String cronExpression, String description) throws SchedulerException, IllegalArgumentException;
 
-    void updateTimetableEntry(Long id, String name, String cronExpression, String description) throws NoSuchElementException;
+    void updateTimetableEntry(Long id, String name, String cronExpression, String description) throws NoSuchElementException, SchedulerException;
 
-    void deleteTimetableEntry(Long id) throws NoSuchElementException;
+    boolean deleteTimetableEntry(Long id) throws NoSuchElementException, SchedulerException;
 
-    void pauseJob(Long id) throws NoSuchElementException;
+    void pauseJob(Long id) throws NoSuchElementException, SchedulerException, IllegalStateException;
 
-    void resumeJob(Long id) throws NoSuchElementException, IllegalStateException;
+    void resumeJob(Long id) throws NoSuchElementException, IllegalStateException, SchedulerException;
 
-    void startJob(Long id) throws NoSuchElementException, IllegalStateException;
+    void startJob(Long id) throws NoSuchElementException, IllegalStateException, SchedulerException;
+
+    List<TimetableEntryDTO.Response.TimetableEntry> getAllEntries();
+
+    TimetableEntryDTO.Response.TimetableEntry getEntryById(Long id) throws NoSuchElementException;
 }
