fb08748841055c2ccfb22608f6ca35b558c540be
Zavar30 <yarusking@mail.ru>
2023-12-12 00:34:54 +0300

    sql for devices init

--- facc-back/facc-control/src/main/resources/data.sql
+++ facc-back/facc-control/src/main/resources/data.sql
@@ -12,7 +12,8 @@ INSERT INTO actions (id, action, action_group, arguments_count) VALUES
                                                                     (4, 'curtains_up', 'CURTAINS_GROUP', 0),
                                                                     (5, 'curtains_down', 'CURTAINS_GROUP', 0),
                                                                     (6, 'music_play', 'MUSIC_GROUP', 0),
-                                                                    (7, 'music_stop', 'MUSIC_GROUP', 0)
+                                                                    (7, 'music_stop', 'MUSIC_GROUP', 0),
+                                                                    (8, 'music_volume', 'MUSIC_GROUP', 1)
                                                                 ON CONFLICT DO NOTHING;
 
 INSERT INTO commands_for_actions (id, command, action_id, file_id) VALUES
@@ -22,4 +23,11 @@ INSERT INTO commands_for_actions (id, command, action_id, file_id) VALUES
                                                                        (4, 'Опусти шторы', 5, null),
                                                                        (5, 'Включи музыку', 6, null),
                                                                        (6, 'Выключи музыку', 7, null)
+                                                                ON CONFLICT DO NOTHING;
+
+INSERT INTO devices (id, name, status, type_id, job_group) VALUES
+                                                    ('fdgpiub234bx', 'LightDevice1', false, 1, 'LIGHT_GROUP'),
+                                                    ('df14sdf124as', 'CurtainsDevice1', false, 2, 'CURTAINS_GROUP'),
+                                                    ('vnbojfdg2425', 'SpeakerDevice1', false, 3, 'SPEAKERS_GROUP'),
+                                                    ('plpyiyuob242', 'MusicDevice1', false, 4, 'MUSIC_GROUP')
                                                                 ON CONFLICT DO NOTHING;
