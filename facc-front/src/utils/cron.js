ac34fa1741214a6de58d3280921b954e18a35fa5
Danila Pimenov <steamaccout111@mail.ru>
2023-12-18 22:11:50 +0300

    Cron fixes#3

--- facc-front/src/utils/cron.js
+++ facc-front/src/utils/cron.js
@@ -22,6 +22,30 @@ const getDayOfWeekName = (dayOfWeekNumber) => {
       return '*';
   }
 };
+
+const getDayOfWeekNameReverse = (dayOfWeekNumber) => {
+  switch (dayOfWeekNumber) {
+    case _default:
+      return 'error';
+    case 2:
+      return 0;
+    case 3:
+      return 1;
+    case 4:
+      return 2;
+    case 5:
+      return 3;
+    case 6:
+      return 4;
+    case 7:
+      return 5;
+    case 1:
+      return 6;
+    case '*':
+      return 7;
+  }
+};
+
 export let generateCronExpression = (timetableRow) => {
   return [
     '? ' + timetableRow.from.split(':')[1] + ' ' +
@@ -36,7 +60,7 @@ export let generateTimeSlot = (expressionFrom, expressionTo) => {
   let on = expressionFrom.split(' ');
   let off = expressionTo === undefined ? null : expressionTo.split(' ');
   return {
-    dayOfWeek: on[5] - 2 >= 0 ? on[5] - 2 : 7,
+    dayOfWeek: getDayOfWeekNameReverse(on[5]),
     from: on[2] + ":" + on[1],
     to: expressionTo === undefined ? null : off[2] + ":" + off[1]
   };
