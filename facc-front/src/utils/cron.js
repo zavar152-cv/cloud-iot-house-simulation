3eaa7c99ff0e7289584a96b81f9c9180d8b11721
Danila Pimenov <steamaccout111@mail.ru>
2023-12-18 22:18:16 +0300

    Cron fixes#5

--- facc-front/src/utils/cron.js
+++ facc-front/src/utils/cron.js
@@ -27,19 +27,19 @@ const getDayOfWeekNameReverse = (dayOfWeekNumber) => {
   switch (dayOfWeekNumber) {
     case _default:
       return 'error';
-    case 2:
+    case "2":
       return 0;
-    case 3:
+    case "3":
       return 1;
-    case 4:
+    case "4":
       return 2;
-    case 5:
+    case "5":
       return 3;
-    case 6:
+    case "6":
       return 4;
-    case 7:
+    case "7":
       return 5;
-    case 1:
+    case "1":
       return 6;
     case '*':
       return 7;
