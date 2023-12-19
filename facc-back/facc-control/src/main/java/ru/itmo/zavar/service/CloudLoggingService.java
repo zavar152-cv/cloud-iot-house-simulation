c7bc3ea74203a970a005278ab95d2e4c514abac9
Zavar30 <yarusking@mail.ru>
2023-12-19 22:36:15 +0300

    FINALLY FIX OF YANDEX SDK TO USE CLOUD LOGGING

--- facc-back/facc-control/src/main/java/ru/itmo/zavar/service/CloudLoggingService.java
+++ facc-back/facc-control/src/main/java/ru/itmo/zavar/service/CloudLoggingService.java
@@ -4,7 +4,7 @@ import yandex.cloud.api.logging.v1.LogEntryOuterClass;
 import yandex.cloud.api.logging.v1.LogIngestionServiceOuterClass;
 
 public interface CloudLoggingService {
-    LogIngestionServiceOuterClass.WriteResponse log(LogEntryOuterClass.LogLevel.Level level, String resourceId, String message, Object... arguments);
+    void log(LogEntryOuterClass.LogLevel.Level level, String resourceId, String message, Object... arguments);
 
-    LogIngestionServiceOuterClass.WriteResponse log(LogEntryOuterClass.LogLevel.Level level, String resourceId, String message, Throwable throwable);
+    void log(LogEntryOuterClass.LogLevel.Level level, String resourceId, String message, Throwable throwable);
 }
